bff7ab837f23c1e2a5060b6eb3bd8c7bfaad5658
Nicolás Godoy <nicolasgastongodoy@gmail.com>
2023-09-28 01:34:00 -0300

    Creé componente, cambié colores, acomodé secciones

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,6 @@ import './index.css';
 import './App.css';
 import { AiOutlineWhatsApp } from 'react-icons/ai';
 import background from './assets/home-background.jpg';
-import Corazon from './assets/etiqueta_corazon.svg';
-import Consultas from './assets/consultas_icono.svg';
-import HoldingHeart from './assets/hands_holding_heart_icono.svg';
-import HandShake from './assets/handshake_icon.png';
 
 import handshake from './assets/handshake.svg';
 import tag_heart from './assets/tag_heart.svg';
@@ -15,34 +11,47 @@ import headset from './assets/headset.svg';
 import { Navbar } from './components/navbar/navbar';
 import { HomeCard } from './components/HomeCard/HomeCard';
 import { HomeButton } from './components/HomeButton/HomeButton';
-import Section from './components/Sections/Section';
+// import Section from './components/Sections/Section';
 import ScrollButton from './components/Scroll/ScrollButton';
 import Products from './components/Sections/Products';
 import Form from './components/Form/Form';
 
+import { RequisitosCard } from './components/RequisitosCard/RequisitosCard';
+
+import imageUrl from '../src/assets/ruler.svg';
+import imageUrl2 from '../src/assets/etiqueta.svg';
+import imageUrl3 from '../src/assets/rulermeasure.svg';
+import imageUrl4 from '../src/assets/change.svg';
+import imageUrl5 from '../src/assets/tear.svg';
+
 function App() {
+
+
   return (
     <div className="w-full">
+
       <ScrollButton />
       <header className="relative z-30 h-[110px] w-full mt-0">
         <Navbar />
       </header>
 
-      <section className="relative z-20 mb-[50px] mt-[150px] ps-[220px]">
-        <h1 className=" leading-[100px] h-[250px] w-[600px] text-gray-200 text-8xl font-bold font-['Roboto', 'sans_serif']">
+      <section className="relative z-20 mb-[60px] mt-[70px] ps-[150px]">
+        
+        <h1 className=" leading-[100px] h-[200px] w-[600px] text-gray-200 text-[80px] font-bold font-['Roboto', 'sans_serif']">
           Etiquetas
           <br />a medida
         </h1>
 
-        <p className=" mb-10 w-473 leading-tight text-white text-lg font-normal font-['Roboto', 'sans_serif']">
+        <p className=" mb-5 w-473 leading-tight text-white text-md font-normal font-['Roboto', 'sans_serif']">
           La etiqueta perfecta para cada producto. Explora nuestras <br />
           etiquetas impresas a medida y destaca en el mercado.
         </p>
 
         <HomeButton titulo={'Ver servicios'} />
+
       </section>
 
-      <section className=" h-[350px] w-[1200] relative z-20 flex items-center align-items justify-center gap-5 ">
+      <section className=" h-[200px] w-[1200] relative z-20 flex items-center align-items justify-center gap-5 ">
         <HomeCard titulo={'Somos profesionales'} imagen={handshake} />
 
         <HomeCard titulo={'Nos Gusta lo que hacemos'} imagen={tag_heart} />
@@ -52,22 +61,54 @@ function App() {
         <HomeCard titulo={'Atentos a tus consultas'} imagen={headset} />
       </section>
 
-      <section className="grid place-items-center bg-[#25D366] w-[46px] h-[46px] z-20 fixed bottom-[30px] right-[2.35rem] rounded-full hover:w-[100px] hover:ease-in-out hover:duration-300">
-        <div className="grid place-items-center align-items-center w-[46px] h-[46px]">
-          <AiOutlineWhatsApp className="p-2 text-white text-[46px]" />
-        </div>
-      </section>
 
-      <div className="absolute top-0 bottom-0 h-full w-full z-10">
+      <div className='relative z-30'>
+        <section className="grid place-items-center bg-[#25D366] w-[46px] h-[46px] z-25 fixed bottom-[30px] right-[2.35rem] rounded-full hover:w-[100px] hover:ease-in-out hover:duration-300">
+          <div className="grid place-items-center align-items-center w-[46px] h-[46px]">
+            <AiOutlineWhatsApp className="p-2 text-white text-[46px]" />
+          </div>
+        </section>
+      </div>
+
+
+      <div className="absolute top-0 bottom-0 h-[700px] w-full z-10">
         <img
           src={background}
           className="absolute top-0 bottom-0 w-full z-10 h-[850px]"
           alt="Vite logo"
         />
       </div>
-      <Section />
-      <Products />
-      <Form />
+      
+      {/* <Section /> */}
+      <div className=' relative z-20 bg-[#E8E8E8]'>
+
+        <div className='flex items-center justify-center pt-[50px]'>
+          <div className=" text-blue-950 text-[40px] font-bold font-['Roboto', sans_serif] text-[#16284E]">
+            <h2>Requisitos</h2>
+            <div className="my-[1px] border-b-[3px] border-[#5BB4EB] w-[164px] mx-auto"></div>
+          </div>
+
+        </div>
+
+        <section className="  h-[430px] w-full flex items-center align-items justify-center gap-5 mx-4 ">
+          <RequisitosCard titulo={'Ancho 110mm'} imagen={imageUrl} />
+
+          <RequisitosCard titulo={'Color de etiqueta a eleccion'} imagen={imageUrl2} />
+
+          <RequisitosCard titulo={'Largo ilimitado'} imagen={imageUrl3} />
+
+          <RequisitosCard titulo={'Textiles y autoadhesivas'} imagen={imageUrl4} />
+
+          <RequisitosCard titulo={'Unico color'} imagen={imageUrl5} />
+
+        </section>
+      </div>
+      
+      <div className='relative z-20 bg-[#E8E8E8]'>
+        <Products className='' />
+        <Form />
+      </div>
+
     </div>
   );
 }
--- src/components/Sections/Products.jsx
+++ src/components/Sections/Products.jsx
@@ -2,12 +2,12 @@ import Cards from '../Cards/Cards';
 
 const Products = () => {
   return (
-    <section className="w-full h-256 bg-gray-200 -mt-2">
-      <div className="text-center text-blue-950 text-[40px] font-bold font-['Roboto', sans_serif] mt-2">
+    <section className="w-full h-256 bg-[#E8E8E8] -mt-2">
+      <div className="text-center text-[#16284E] text-[40px] font-bold font-['Roboto', sans_serif] mt-2">
         Nuestros Productos
-        <div className="my-[1px] border-b-[3px] border-blue-400 w-[295px] mx-auto"></div>
-        <div className="-mt-0.5 border-b-[3px] border-gray-400 w-[295px] mx-auto"></div>
-        <p className="mt-4 mb-6 text-cyan-900 text-lg font-normal font-['Roboto, sans_serif']">
+        <div className="my-[1px] border-b-[3px] border-[#5BB4EB] w-[295px] mx-auto"></div>
+        {/* <div className="-mt-0.5 border-b-[3px] border-gray-400 w-[295px] mx-auto"></div> */}
+        <p className="mt-4 mb-6 text-[#24485E] text-lg font-normal font-['Roboto, sans_serif']">
           Dependiendo del material de la etiqueta y del ribbon, ofrecemos
           productos en <br /> distintas calidades, segun tus necesidades y
           aplicaciones:
