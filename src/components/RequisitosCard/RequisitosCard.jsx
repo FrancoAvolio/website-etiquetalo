dd82085004e21eec0f6888ba239b3ec3797d4a2b
FrancoAvolio <francoavolio@hotmail.com>
2023-09-28 13:47:43 -0300

    '

--- src/components/RequisitosCard/RequisitosCard.jsx
+++ src/components/RequisitosCard/RequisitosCard.jsx
@@ -4,7 +4,7 @@ export const RequisitosCard = ({titulo, imagen}) => {
 
 
   return (
-    <div className="flex justify-center mt-5 gap-8 mb-5">
+    <div className="flex justify-center mt-5 gap-8 mb-5 rounded-t-[20px]">
       <div className="w-48 h-[170px] bg-white rounded-[20px] shadow-xl shadow-gray-400 border-2 border-white">
         <div className="">
           <img className="mx-auto mt-5 w-[90px] h-[70px]" src={imagen} />
