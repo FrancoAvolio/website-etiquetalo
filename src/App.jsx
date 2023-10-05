52ae230f58c62e7ce604b812e6880854193b7a5b
Nicolás Godoy <nicolasgastongodoy@gmail.com>
2023-10-05 01:20:51 -0300

    seccion de porqué elegirnos

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,8 @@ import imageUrl2 from '../src/assets/etiqueta.svg';
 import imageUrl3 from '../src/assets/rulermeasure.svg';
 import imageUrl4 from '../src/assets/change.svg';
 import imageUrl5 from '../src/assets/tear.svg';
+import imagenElegirnos from '../src/assets/trabajador_con_etiquedadora.png';
+import cajaCeleste from './../src/assets/caja_celeste.png'
 
 import { Drawer } from '@material-tailwind/react';
 import DrawerAppBar from './components/Bar/DrawerAppBar';
@@ -34,17 +36,17 @@ function App() {
     <div className="w-full">
       <ScrollButton />
       <header className="relative z-30 h-[6.875rem] w-full mt-0">
-        {/* <Navbar /> */}
-        <DrawerAppBar />
+        <Navbar />
+        {/* <DrawerAppBar /> */}
       </header>
-      
-      <section className="relative z-20 sm:mb-20 sm:mt-[4.375rem] sm:ps-20 lg:ps-40 flex flex-col sm:flex-wrap max-sm:flex-wrap  flex-wrap h-auto ps-4 mb-10 text-xs my-10">
+
+      <section className="relative z-20 sm:mb-20 sm:mt-[4.375rem] sm:ps-20 lg:ps-40 flex flex-col sm:flex-wrap max-sm:flex-wrap  flex-wrap h-auto ps-4 mb-10 text-xs my-10 gap-4">
         
-        <h1 className="sm:mb-4 sm:leading-[4.125rem] sm:h-[8.438rem] text-gray-200 sm:text-[3.75rem] font-bold font-['Roboto', 'sans_serif'] h-auto text-5xl my-4">
+        <h1 className="sm:leading-[4.125rem] sm:h-[8.438rem] text-gray-200 sm:text-[3.75rem] font-bold font-['Roboto', 'sans_serif'] h-auto text-5xl">
           Impresión de <br />etiquetas a medida
         </h1>
 
-        <p className="sm:mb-5 sm:leading-tight text-white sm:text-md font-normal font-['Roboto', 'sans_serif'] text-xs mb-3">
+        <p className="sm:leading-tight sm:text-lg text-white sm:text-md font-normal font-['Roboto', 'sans_serif'] text-xs">
           La etiqueta perfecta para cada producto. Explora nuestras <br />
           etiquetas impresas a medida y destaca en el mercado.
         </p>
@@ -52,7 +54,7 @@ function App() {
         <HomeButton titulo={'Ver servicios'} />
       </section>
 
-      <section className="md:h-[12.5rem] sm:h-auto sm:p-5 relative z-20 flex items-center align-items justify-center gap-5 sm:my-[1.875rem] sm:flex-wrap sh-auto flex-wrap my-5 px-7">
+      <section className="md:h-[12.5rem] sm:h-auto sm:p-5 relative z-20 flex items-center justify-center gap-5 sm:my-[1.875rem] sm:flex-wrap sh-auto flex-wrap my-5 px-7">
         <HomeCard titulo={'Somos profesionales'} imagen={handshake} />
 
         <HomeCard titulo={'Nos Gusta lo que hacemos'} imagen={tag_heart} />
@@ -107,6 +109,60 @@ function App() {
 
       <div className="bg-[#E8E8E8] 2sm:p-2">
         <Products className="" />
+
+        <div className='flex justify-center flex-col w-full h-[55.625rem] flex-wrap' style={{background: 'linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, #E8E8E8 31%)'}}>
+          <div className="text-center text-[#16284E] text-[2.5rem] font-bold font-['Roboto', sans_serif] mt-2 mb-11 flex-wrap">
+            ¿Por qué elegirnos?
+            <div className="my-[1px] border-b-[3px] border-[#5BB4EB] w-[295px] mx-auto"></div>
+          </div>
+
+          <div className='flex justify-center items-center gap-[130px]'>
+            <ul className='flex flex-col gap-[70px] justify-between'>
+
+              <div className='relative w-[31.25rem] h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[5rem] pe-4' style={{background:'rgba(61, 121, 158, 0.30)',}}>
+                <h2 className="absolute top-[-12.5%] left-[13%] text-[#16284E] text-[26px] font-semibold font-['Roboto', 'sans_serif'] leading-7">Te asesoramos</h2>
+                <div className='absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center'>
+                  <img className='h-full w-full' src={cajaCeleste} alt="" />
+                  <div className='absolute translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold' style={{textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>1</div> 
+                </div>
+                <ul className='list-disc'>
+                  <li className='text-[16px] leading-tigli font-normal font-["Roboto", "sans_serif"]'>Te ayudamos a encontrar la mejor etiqueta para tus productos.</li>
+                  <li className='text-[16px] leading-tigli font-normal font-["Roboto", "sans_serif"]'>Definimos la etiqueta según tus necesidades de control de inventario, incluyendo tipo de código, longitud de código, y tipo de etiqueta.</li>
+                </ul>
+              </div>
+
+              <div className='relative w-[31.25rem] h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[5rem] pe-4' style={{background:'rgba(61, 121, 158, 0.30)',}}>
+                <h2 className="absolute top-[-12.5%] left-[13%] text-[#16284E] text-[26px] font-semibold font-['Roboto', 'sans_serif'] leading-7">Pronta entrega</h2>
+                <div className='absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center'>
+                  <img className='h-full w-full' src={cajaCeleste} alt="" />
+                  <div className='absolute translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold' style={{textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>2</div> 
+                </div>
+                <ul className='list-disc'>
+                  <li className='text-base leading-tigli font-normal font-["Roboto", "sans_serif"]'>Garantizamos una entrega rápida y puntual de tus etiquetas.</li>
+                  <li className='text-[16px] leading-tigli font-normal font-["Roboto", "sans_serif"]'>Nos comprometemos a mantener tus proyectos en tiempo y forma.</li>
+                </ul>
+                
+              </div>
+
+              <div className='relative w-[31.25rem] h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[5rem] pe-4' style={{background:'rgba(61, 121, 158, 0.30)',}}>
+                <h2 className="absolute top-[-12.5%] left-[13%] text-[#16284E] text-[26px] font-semibold font-['Roboto', 'sans_serif'] leading-7">Servicio de etiquetado</h2>
+                <div className='absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center'>
+                  <img className='h-full w-full' src={cajaCeleste} alt="" />
+                  <div className='absolute translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold' style={{textShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'}}>3</div> 
+                </div>
+                <ul className='list-disc'>
+                  <li className='text-[16px] leading-tigli font-normal font-["Roboto", "sans_serif"]'>Ofrecemos un servicio completo de etiquetado para simplificar tu proceso de etiquetado.</li>
+                  <li className='text-[16px] leading-tigli font-normal font-["Roboto", "sans_serif"]'>Nuestro equipo experto se encarga de etiquetar tus productos de manera precisa y eficiente.</li>
+                </ul>
+                
+              </div>
+
+            </ul>
+            <img src={imagenElegirnos} alt="" />
+          </div>
+          
+        </div>
+
         <Form />
       </div>
     </div>
