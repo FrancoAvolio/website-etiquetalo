7d5f76546ace36be29f793fceefb1cd764af2958
Nicolás Godoy <nicolasgastongodoy@gmail.com>
2023-10-26 00:02:44 -0300

    cambios tamaña fuenta de titulos tarjtas elegirnos

--- src/components/ElegirnosSection/ElegirnosSection.jsx
+++ src/components/ElegirnosSection/ElegirnosSection.jsx
@@ -18,12 +18,12 @@ export const ElegirnosSection = () => {
 
         <ul className="flex flex-col gap-[2.5rem] justify-between sm:w-auto w-full h-auto lg:min-w-[400px] lg:w-[35rem] lg:m-0">
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px]  text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] sm:text-[1.5rem] font-semibold font-['Roboto', 'sans_serif'] leading-7 w-full text-base sm:ms-0 ms-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Te asesoramos
             </h2>
-            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center">
+            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
               <img className="h-full w-full" src={cajaCeleste} alt="" />
-              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold">
+              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] sm:text-4xl text-2xl font-bold">
                 1
               </div>
             </div>
@@ -40,12 +40,12 @@ export const ElegirnosSection = () => {
           </div>
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] sm:text-[1.5rem] font-semibold font-['Roboto', 'sans_serif'] leading-7 w-full text-base sm:ms-0 ms-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Pronta entrega
             </h2>
-            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center">
+            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
               <img className="h-full w-full" src={cajaCeleste} alt="" />
-              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold">
+              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] sm:text-4xl text-2xl font-bold">
                 2
               </div>
             </div>
@@ -62,12 +62,12 @@ export const ElegirnosSection = () => {
 
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] sm:text-[1.5rem] font-semibold font-['Roboto', 'sans_serif'] leading-7 w-full text-base sm:ms-0 ms-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Diseño
             </h2>
-            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center">
+            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
               <img className="h-full w-full" src={cajaCeleste} alt="" />
-              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold">
+              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] sm:text-4xl text-2xl font-bold">
                 3
               </div>
             </div>
@@ -83,12 +83,12 @@ export const ElegirnosSection = () => {
           </div>
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] sm:text-[1.5rem] font-semibold font-['Roboto', 'sans_serif'] leading-7 w-full text-base sm:ms-0 ms-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Consultanos por otros servicios
             </h2>
-            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] h-[4.5rem] w-[4.5rem] grid items-center">
+            <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
               <img className="h-full w-full" src={cajaCeleste} alt="" />
-              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] text-4xl font-bold">
+              <div className="absolute sombra translate-x-[120%] translate-y-[-10%] text-center text-[#F8F8F8] sm:text-4xl text-2xl font-bold">
                 4
               </div>
             </div>
