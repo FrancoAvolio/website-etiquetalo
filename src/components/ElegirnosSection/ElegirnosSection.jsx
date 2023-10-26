bae77bcfe5ea78abb8b9b9ea474d98dd122c553a
Nicolás Godoy <nicolasgastongodoy@gmail.com>
2023-10-26 00:13:38 -0300

    1 rem titulo

--- src/components/ElegirnosSection/ElegirnosSection.jsx
+++ src/components/ElegirnosSection/ElegirnosSection.jsx
@@ -18,7 +18,7 @@ export const ElegirnosSection = () => {
 
         <ul className="flex flex-col gap-[2.5rem] justify-between sm:w-auto w-full h-auto lg:min-w-[400px] lg:w-[35rem] lg:m-0">
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px]  text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-[1rem] sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Te asesoramos
             </h2>
             <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
@@ -40,7 +40,7 @@ export const ElegirnosSection = () => {
           </div>
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-[1rem] sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Pronta entrega
             </h2>
             <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
@@ -62,7 +62,7 @@ export const ElegirnosSection = () => {
 
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-[1rem] sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Diseño
             </h2>
             <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
@@ -83,7 +83,7 @@ export const ElegirnosSection = () => {
           </div>
 
           <div className="relative bg-[#FFF] sm:w-full md:h-[8.75rem] rounded-tr-[20px] rounded-bl-[20px] text-[#16284E] grid items-center py-4 ps-[6rem] pe-3 w-full h-auto">
-            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-sm sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
+            <h2 className="absolute top-[-19%] left-[12%] text-[#16284E] text-[1rem] sm:text-[1.4rem] font-semibold font-['Roboto', 'sans_serif'] w-full sm:ms-0 2sm:ms-5 ms-6 md:mb-1 2sm:mt-1 mt-3">
               Consultanos por otros servicios
             </h2>
             <div className="absolute top-0 left-0 translate-x-[-25%] translate-y-[-25%] sm:h-[4.5rem] sm:w-[4.5rem] h-[3rem] w-[3rem] grid items-center">
